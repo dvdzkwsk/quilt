481a6e963e101eab44ce9d2592c8adebd28123b4
David Zukowski <6439050+davezuko@users.noreply.github.com>
2024-03-22 23:54:15 -0700

    basic note parser

--- pkg/parser/Parser.test.ts
+++ pkg/parser/Parser.test.ts
@@ -1,9 +1,36 @@
 import {expect, test} from "bun:test"
-import {parseNoteLiteral} from "./parser.js"
+import {parseNoteLiteral} from "./Parser.js"
 
-test("parseNoteLiteral > empty input returns an empty note", () => {
+test("parseNoteLiteral", () => {
 	expect(parseNoteLiteral("")).toEqual({
-		text: "",
-		tags: [],
+		note: {
+			text: "",
+			tags: [],
+			timestamp: null,
+		},
+	})
+
+	expect(parseNoteLiteral('"hello"')).toEqual({
+		note: {
+			text: "hello",
+			tags: [],
+			timestamp: null,
+		},
+	})
+
+	expect(parseNoteLiteral('"hello world"')).toEqual({
+		note: {
+			text: "hello world",
+			tags: [],
+			timestamp: null,
+		},
+	})
+
+	expect(parseNoteLiteral('"hello world" +foo +bar')).toEqual({
+		note: {
+			text: "hello world",
+			tags: [{name: "foo"}, {name: "bar"}],
+			timestamp: null,
+		},
 	})
 })
--- pkg/parser/Parser.ts
+++ pkg/parser/Parser.ts
@@ -1,16 +1,84 @@
 export interface NoteLiteral {
 	text: string
 	tags: TagLiteral[]
+	timestamp: string | null
 }
 
 export interface TagLiteral {
 	name: string
 }
 
-export function parseNoteLiteral(input: string): NoteLiteral {
+export interface ParseResult {
+	note: NoteLiteral
+}
+export function parseNoteLiteral(input: string): ParseResult {
 	const note: NoteLiteral = {
 		text: "",
 		tags: [],
+		timestamp: null,
 	}
-	return note
+
+	let cursor = 0
+
+	function parseTimestamp(): string {
+		cursor++
+
+		let timestamp = ""
+		while (cursor < input.length) {
+			const char = input[cursor++]
+			if (char === "]") {
+				return timestamp
+			}
+			timestamp += char
+		}
+		throw new Error("unterminated timestamp literal")
+	}
+
+	function parseText(): string {
+		cursor++
+
+		let text = ""
+		while (cursor < input.length) {
+			const char = input[cursor++]
+			if (char === '"') {
+				return text
+			}
+			text += char
+		}
+		throw new Error("unterminated text literal")
+	}
+
+	function parseTag(): TagLiteral {
+		cursor++
+
+		const tag: TagLiteral = {name: ""}
+		while (cursor < input.length) {
+			const char = input[cursor++]
+			if (char === " ") {
+				return tag
+			}
+			tag.name += char
+		}
+		return tag
+	}
+
+	while (cursor < input.length) {
+		switch (input[cursor]) {
+			case " ":
+				cursor++
+				break
+			case "[":
+				note.timestamp = parseTimestamp()
+				break
+			case '"':
+				note.text = parseText()
+				break
+			case "+":
+				const tag = parseTag()
+				note.tags.push(tag)
+				break
+		}
+	}
+
+	return {note}
 }
